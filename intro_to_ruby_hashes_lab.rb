ddab1cfbdd3918679c286144307111c349cd9f60
Shayla White <shayladwhite1@gmail.com>
2019-05-29 02:09:48 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -30,7 +30,8 @@ monopoly  =
 	  :pieces => 4,
 	  :names => {:reading_railroad => {},
 	             :pennsylvania_railroad => {},
-	             
+	             :b_and_o_railroad => {},
+	             :shortline => {}
 	  },
 	  :rent_in_dollars => {:one_piece_owned => 25,
 	                       :two_pieces_owned => 50,
